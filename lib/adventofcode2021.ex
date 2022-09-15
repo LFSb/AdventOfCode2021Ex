023a3db4a25bf97a1a2e67a9c6dbedd46979600b
Lee Beenen <leebeenen@gmail.com>
2022-09-15 13:28:22 +0200

    Rewrite, implement p2.

--- lib/adventofcode2021.ex
+++ lib/adventofcode2021.ex
@@ -1,18 +1,15 @@
 defmodule Adventofcode2021 do
-  use Application
   import Helpers
 
-  def day1(input) do
-    input 
-    |> Enum.with_index 
-    |> Enum.count(fn({_val, idx}) -> comparePrevious(input, idx) == true end)
+  def day1p1(input) do
+    input
+    |> Enum.with_index()
+    |> Enum.count(fn {_val, idx} -> isValueLargerThanPreviousIdx(input, idx) == true end)
   end
 
-  def start(_type, _args) do
-    input = File.read!("lib/inputs/day1.txt")
-            |> String.split("\n")
-            |> Enum.map(&String.to_integer/1)
-    
-    IO.puts(day1(input))
+  def day1p2(input) do
+    input
+    |> Enum.with_index()
+    |> Enum.count(fn {_val, idx} -> isWindowLargerThanPreviousWindow(input, idx) == true end)
   end
 end
