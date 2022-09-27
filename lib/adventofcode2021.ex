9b4e8e29dfb8f4dc054ab399f56d814cf8b857f0
Lee Beenen <leebeenen@gmail.com>
2022-09-27 14:48:05 +0200

    Remove the call that is now incorrect due to the method signature changing.

--- lib/adventofcode2021.ex
+++ lib/adventofcode2021.ex
@@ -35,6 +35,6 @@ defmodule Adventofcode2021 do
   end
   
   def day3p2(input) do
-    input |> filterInputByIndex(fn f -> f |> Map.keys |> length > 1 end) #TODO: This function is not optimal. It works up until where there's an equal occurance for characters in the frequency map, then it breaks down.
+    input 
   end
 end
