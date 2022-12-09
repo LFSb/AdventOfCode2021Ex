7c93982b283c5f4c58c5580f205eabc045356c6c
Lee beenen <lee.beenen@ns.nl>
2022-12-09 21:09:18 +0100

    Slight refactor

--- lib/structs.ex
+++ lib/structs.ex
@@ -7,16 +7,23 @@ defmodule File2 do
 end
 
 defmodule Day7 do
-  def sum_files([]) do
-    0
+  @moduledoc """
+  This just solves Day 7, as long as you provide the directory structure as a list of Dir's containing subsequent children. 
+  """
+  def p1(list) do
+    p1(list, 0)
   end
 
-  def sum_files([%File2{} = head | tail]) do
-    sum_files(tail) + head.size
+  defp p1([], sum) do
+    sum
   end
 
-  def sum_files([%Dir{} = head | tail]) do
-    dir_size = sum_files(head.children) + sum_files(tail)
+  defp p1([%{name: _, size: _} = head | tail], sum) do
+    p1(tail, sum) + head.size + sum
+  end
+
+  defp p1([%{path: _, children: _} = head | tail], sum) do
+    dir_size = p1(head.children, sum) + p1(tail, sum)
     if dir_size < 100_000, do: dir_size, else: 0
   end
 end
