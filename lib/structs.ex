c7b7f830628ca06ace133391f48cf6fd0b2f44cb
Lee beenen <lee.beenen@ns.nl>
2022-12-12 08:35:02 +0100

    WIP Day7

--- lib/structs.ex
+++ lib/structs.ex
@@ -26,4 +26,22 @@ defmodule Day7 do
     dir_size = p1(head.children, sum) + p1(tail, sum)
     if dir_size < 100_000, do: dir_size, else: 0
   end
+
+  def add_children(line) do
+    line |> Enum.flat_map(fn x -> x |> parse_children end)
+  end
+  
+  def parse_children(grouping) do
+    starts_with_dir = elem(grouping, 0)
+
+    if starts_with_dir do
+      elem(grouping, 1)
+      |> Enum.map(fn x -> x |> String.split(" ") end)
+      |> Enum.map(fn y -> %Dir{path: Enum.at(y, 1)} end)
+    else
+      elem(grouping, 1)
+      |> Enum.map(fn x -> x |> String.split(" ") end)
+      |> Enum.map(fn y -> %File2{name: Enum.at(y, 1), size: String.to_integer(Enum.at(y, 0))} end)
+    end
+  end
 end
