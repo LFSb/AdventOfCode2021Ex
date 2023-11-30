20587567421261dae112b03d0abf5c471acd944d
Lee Beenen <lee.beenen@dhl.com>
2023-11-30 10:23:07 +0100

    Set up 2023

---
+++ lib/adventofcode2023.ex
@@ -0,0 +1,4 @@
+defmodule Adventofcode2023 do
+  def day1p1(input) do
+  end
+end
---
+++ test/2023/adventofcode2023_test.exs
@@ -0,0 +1,6 @@
+defmodule Adventofcode2023Test do
+  import FileUtils
+  import Helpers
+  use ExUnit.Case
+  doctest Adventofcode2023
+end
