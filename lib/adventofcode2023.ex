37489f2721af3c603849de91305f310450c5e74a
Lee Beenen <lee.beenen@dhl.com>
2023-12-01 14:24:45 +0100

    First go at p2, works on test input...but not on the real input. What gives?

--- lib/adventofcode2023.ex
+++ lib/adventofcode2023.ex
@@ -6,4 +6,25 @@ defmodule Adventofcode2023 do
     |> Enum.map(&String.to_integer/1)
     |> Enum.sum()
   end
+
+  def day1p2(input) do
+    words = %{"one" => "1", "two" => "2", "three" => "3", "four" => "4", "five" => "5", "six" => "6", "seven" => "7", "eight" => "8", "nine" => "9"}
+    numbers = Map.new(words, fn {key, val} -> {val, key} end)
+    dict = Map.merge(words, numbers)
+    keys = dict |> Map.keys
+
+    input
+    |> Enum.map(fn line ->
+      keys
+      |> Enum.filter(fn key -> String.contains?(line, key) end)
+      |> Enum.sort_by(fn occ -> String.split(line, occ) |> List.first() |> String.length end)
+    end)
+    |> Enum.map(fn line ->
+      line
+      |> Enum.map(fn el -> if Map.has_key?(words, el), do: words[el], else: el end)
+    end)
+    |> Enum.map(fn digits -> List.first(digits) <> List.last(digits) end)
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.sum()
+  end
 end
--- test/2023/adventofcode2023_test.exs
+++ test/2023/adventofcode2023_test.exs
@@ -11,4 +11,12 @@ defmodule Adventofcode2023Test do
   test "day1p1 realinput" do
     assert readSplitLinesForDay("1", "2023") |> Adventofcode2023.day1p1() == 54239
   end
+
+  test "day1p2 testinput" do
+    assert readSplitLinesForDay("1test2", "2023") |> Adventofcode2023.day1p2() == 281
+  end
+
+  test "day1p2 realinput" do
+    assert readSplitLinesForDay("1", "2023") |> Adventofcode2023.day1p2() == 55355
+  end
 end
